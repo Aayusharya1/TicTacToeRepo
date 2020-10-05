74c43c091fab1d8415c1873e052e48e28b6be14d
aayusharya1 <aayusharya07@gmail.com>
2020-10-05 15:16:42 +0530

    [Aayush] Add. Giving option x or y to user

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,15 +4,33 @@ namespace TicTacToe
 {
     class TicTacToeGame
     {
-     
+
         static void Main(string[] args)
         {
-            char[] board = new char[10];
+            char[] board;
 
-            for (int i=1;i<10;i++) 
+            board = Createboard();
+        }
+
+        private static char[] Createboard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < 10; i++)
             {
                 board[i] = ' ';
             }
+
+            return board;
+        }
+
+        private void Choosexoro()
+        {
+            char compOption;
+            char playerOption = Convert.ToChar(Console.ReadLine());
+            if (playerOption == 'x')   compOption = 'o'; 
+            else compOption = 'x';
+        
+
         }
     }
 }
